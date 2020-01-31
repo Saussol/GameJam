be70be9acd1c2293e3f2b996fbb336c075bbc94f
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2020-01-31 21:21:50 +0100

    added timer

--- GameJam/Assets/Scripts/Timer.cs
+++ GameJam/Assets/Scripts/Timer.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public float time;
+    public Text timerText;
+    public GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PassingTime();
+    }
+
+    public void PassingTime()
+    {
+        if(time > 0)
+        {
+            time -= Time.deltaTime;
+            timerText.text = time.ToString("F0") + " ";
+            Debug.Log(time);
+        }
+        else if(time <= 0)
+        {
+            Destroy(Player);
+        }
     }
 }
